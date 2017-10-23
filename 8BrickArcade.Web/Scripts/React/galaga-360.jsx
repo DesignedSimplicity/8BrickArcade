fc2a102f0873cb471957e743e19ac827a174cd79
Kevin <Kevin@k2>
2017-10-24 10:12:11 +1300

    Added drag support for viewer image

--- 8BrickArcade.Web/Scripts/React/galaga-360.jsx
+++ 8BrickArcade.Web/Scripts/React/galaga-360.jsx
@@ -1,17 +1,44 @@
 ﻿var GalagaViewer = React.createClass({
     getInitialState: function () {
         return {
-            src: ""
+			src: "",
+			drag: false,
+			start: 0,
+			width: 0
         };
     },
     changeView: function (e) {
         this.setState({
             src: e.target.src.replace("/thumb/", "/")
         });        
-    },
+	},
+	dragStart: function (e) {
+		this.setState({
+			drag: true,
+			start: e.clientX,
+			width: e.target.width
+		});     
+	},
+	dragEnd: function (e) {
+		this.setState({
+			drag: false
+		});     
+	},
+	dragView: function (e) {
+		var delta = e.clientX - this.state.start;
+		var a = (Math.round(8 * delta / this.state.width) % 8) * -45;
+		if (a == -0) a = 0;
+		if (a < 0) a += 360;
+		var angle = a.toString();
+		var degrees = "000".substring(angle.length) + angle;
+		var name = this.props.data.Name.toLowerCase();
+		this.setState({
+			src: "/images/galaga/angles/" + name + "-" + degrees + ".jpg"
+		});
+	},
     render: function () {
-        var name = this.props.data.Name.toLowerCase();
-        if (this.state.src == "") {
+		var name = this.props.data.Name.toLowerCase();
+		if (this.state.src == "") {
             this.state.src = "/images/galaga/angles/" + name + "-000.jpg"
         }
 
@@ -25,8 +52,8 @@
         return (
             <div className="galaga-angles">
                 {angles}
-                <div>
-                    <GalagaViewerZoom name="fighter" src={this.state.src} />
+				<div>
+					<img id="galaga-angles" src={this.state.src} onDrag={this.dragView} onDragStart={this.dragStart} onDragEnd={this.dragEnd} />
                 </div>
             </div>
         );            
@@ -39,14 +66,6 @@ var GalagaViewerAngle = React.createClass({
             <a href="#360" onClick={this.props.onChange} onMouseEnter={this.props.onChange}>
                 <img src={"/images/galaga/angles/thumb/" + this.props.name + "-" + this.props.degrees + ".jpg"} />
             </a>
-        );
-    }
-});
-
-var GalagaViewerZoom = React.createClass({
-    render: function () {
-        return (
-            <img id="galaga-angles" src={this.props.src} />
         );
     }
 });
