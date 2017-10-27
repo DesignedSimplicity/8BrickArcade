a0313d94b4d5a8191f76f23508df627e93861bc3
Kevin <Kevin@X>
2017-10-27 19:45:46 +1300

    Fixed 360 view dragging in Chrome

--- 8BrickArcade.Web/Scripts/React/galaga-360.jsx
+++ 8BrickArcade.Web/Scripts/React/galaga-360.jsx
@@ -1,64 +1,48 @@
 ﻿var GalagaViewer = React.createClass({
     getInitialState: function () {
         return {
-			src: "",
-			drag: false,
 			start: 0,
-			width: 0
+            width: 0,
+            angle: 0,
+            offset: 0
         };
     },
     changeView: function (a) {
         this.setState({
-            src: this.buildUrl(a)
+            angle: a
         });
 	},
-	dragStart: function (e) {
+    dragStart: function (e) {
 		this.setState({
-			drag: true,
 			start: e.clientX,
-			width: e.target.width
-		});     
-	},
-	dragEnd: function (e) {
-		this.setState({
-			drag: false
+            width: e.target.width,
+            offset: this.state.angle
 		});     
 	},
 	dragView: function (e) {
 		var delta = e.clientX - this.state.start;
-		var a = (Math.round(8 * delta / this.state.width) % 8) * -45;
-		if (a == -0) a = 0;
-		if (a < 0) a += 360;
-		var url = this.buildUrl(a);
+        var a = (Math.round(8 * delta / this.state.width) % 8) * -45;
+        a += this.state.offset;
+        if (a < 0) a += 360;
+        if (a >= 360) a -= 360;
+        if (a == -0 || a == 360) a = 0;
 		this.setState({
-			src: url
-		});
-	},
-	buildUrl: function (a) {
-		var name = this.props.data.Name.toLowerCase();
-		var angle = a.toString();
-		var degrees = "000".substring(angle.length) + angle;
-		return "/images/galaga/angles/" + name + "-" + degrees + ".jpg"
+			angle: a
+        });
 	},
     render: function () {
 		var name = this.props.data.Name.toLowerCase();
-		var url = this.buildUrl(0);
-		if (this.state.src == "") {
-			this.state.src = url
-        }
 
         var angles = [];
         for (var a = 0; a < 360; a += 45) {
-            var angle = a.toString();
-            var degrees = "000".substring(angle.length) + angle;
-            angles.push(<GalagaViewerAngle key={a} name={name} degrees={degrees} onChange={this.changeView} />);
+            angles.push(<GalagaViewerAngle key={a} name={name} angle={a} onChange={this.changeView} />);
         }
 
         return (
             <div className="galaga-angles">
 				{angles}
-				<div>
-					<img id="galaga-angles" src={this.state.src} onDrag={this.dragView} onDragStart={this.dragStart} onDragEnd={this.dragEnd} />
+                <div onDragStart={this.dragStart} onDrag={this.dragView} onDragEnd={this.dragView}>
+                    <GalagaViewerAngleImage name={name} angle={this.state.angle} main="true" />
                 </div>
             </div>
         );            
@@ -68,9 +52,25 @@
 var GalagaViewerAngle = React.createClass({
     render: function () {
 		return (
-            <a href="#360" onClick={() => this.props.onChange(this.props.degrees)} onMouseEnter={() => this.props.onChange(this.props.degrees)}>
-                <img src={"/images/galaga/angles/thumb/" + this.props.name + "-" + this.props.degrees + ".jpg"} />
+            <a href="#360" onClick={() => this.props.onChange(this.props.angle)} onMouseOver={() => this.props.onChange(this.props.angle)}>
+                <GalagaViewerAngleImage name={this.props.name} angle={this.props.angle} />
             </a>
+        );
+    }
+});
+
+var GalagaViewerAngleImage = React.createClass({
+    render: function () {
+        var angle = this.props.angle.toString();
+        var degrees = "000".substring(angle.length) + angle;
+
+        var main = this.props.main != undefined;
+        var image = "/images/galaga/angles/";
+        if (!main) image += "thumb/";
+        image += this.props.name + "-" + degrees + ".jpg";
+
+        return (
+            <img draggable={main} src={image} />
         );
     }
 });
