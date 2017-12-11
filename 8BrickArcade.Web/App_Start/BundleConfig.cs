9c71a91a3e25bdd7fa3367b7d3e4a18ef2ac7a66
Kevin <Kevin@k2>
2017-12-12 10:37:28 +1300

    Optimizing config settings

--- 8BrickArcade.Web/App_Start/BundleConfig.cs
+++ 8BrickArcade.Web/App_Start/BundleConfig.cs
@@ -9,23 +9,25 @@ namespace _8BrickArcade
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+			BundleTable.EnableOptimizations = true;
 
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+			//bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
+			//bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-*"));
+			//bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                        "~/Scripts/bootstrap.js"));
+			bundles.Add(new ScriptBundle("~/bundles/js").Include(
+						"~/Scripts/jquery-{version}.js",
+						"~/Scripts/modernizr-*"
+						));
 
-            bundles.Add(new BabelBundle("~/bundles/react").Include(
+			bundles.Add(new BabelBundle("~/bundles/react").Include(
                         "~/Scripts/React/galaga-nav.jsx",
                         "~/Scripts/React/galaga-evo.jsx",
                         "~/Scripts/React/galaga-360.jsx",
 						"~/Scripts/React/galaga-img.jsx",
 						"~/Scripts/React/galaga-row.jsx"
 						));
-
+			
             bundles.Add(new StyleBundle("~/Content/css").Include(
                         "~/Content/bootstrap.css",
                         "~/Content/_main.css",
