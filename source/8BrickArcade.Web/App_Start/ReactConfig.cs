083c7cf0d18d1247cb3c0ce06617389dcf98fd81
KevinWCC <kevin.hutchens@wcc.govt.nz>
2024-03-12 17:17:43 +1300

    Updates

--- source/8BrickArcade.Web/App_Start/ReactConfig.cs
+++ source/8BrickArcade.Web/App_Start/ReactConfig.cs
@@ -8,12 +8,21 @@ namespace _8BrickArcade.Web
 	{
 		public static void Configure()
 		{
-            ReactSiteConfiguration.Configuration
-                .AddScript("~/Scripts/React/galaga-nav.jsx")
-                .AddScript("~/Scripts/React/galaga-evo.jsx")
-                .AddScript("~/Scripts/React/galaga-360.jsx")
-				.AddScript("~/Scripts/React/galaga-img.jsx")
-				.AddScript("~/Scripts/React/galaga-row.jsx");
-        }
+			// If you want to use server-side rendering of React components, 
+			// add all the necessary JavaScript files here. This includes 
+			// your components as well as all of their dependencies.
+			// See http://reactjs.net/ for more information. Example:
+			//ReactSiteConfiguration.Configuration
+			//	.AddScript("~/Scripts/First.jsx")
+			//	.AddScript("~/Scripts/Second.jsx");
+			
+			// If you use an external build too (for example, Babel, Webpack,
+			// Browserify or Gulp), you can improve performance by disabling 
+			// ReactJS.NET's version of Babel and loading the pre-transpiled 
+			// scripts. Example:
+			//ReactSiteConfiguration.Configuration
+			//	.SetLoadBabel(false)
+			//	.AddScriptWithoutTransform("~/Scripts/bundle.server.js")
+		}
 	}
 }
